8e457e476ba40d25cf4389fe0f3ca34ca68c4d35
Roman <roman.tremmel@gmail.com>
2019-12-18 21:13:03 +0100

    Update server.R

--- server.R
+++ server.R
@@ -1,6 +1,5 @@
 server <- function(input, output, session){
-  options(shiny.maxRequestSize = 30*1024^2,
-          shiny.usecairo = TRUE)
+  options(shiny.maxRequestSize = 30*1024^2)
 # dataset -----------------------------------------------------------------
   dataset <- reactive({
     req(input$gene)
